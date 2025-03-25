17afaef6ace28d866e31c144b0d31a7462257e58
BimaEkaSetiawan <bimaekasetiawan1010@gmail.com>
2025-03-25 13:23:49 +0700

    membuat inputan jumlah array

--- insertsort.cpp
+++ insertsort.cpp
@@ -3,3 +3,9 @@ using namespace std;
 
 int arr[20]; // membuat array dengan panjang data 20
 int n; // membuat variable inputan n
+
+void input()
+{
+    cout << "Masukan Jumlah Data pada Array : "; //membuat inputan jumlah element array
+    cin >> n; //memangil variable inputan n
+}
