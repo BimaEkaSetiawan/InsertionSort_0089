32b9675739724cf3ca24488a727ee9de72b31286
BimaEkaSetiawan <bimaekasetiawan1010@gmail.com>
2025-03-26 08:31:18 +0700

    membuat void display

--- insertsort.cpp
+++ insertsort.cpp
@@ -54,3 +54,19 @@ void insertionSort()
     }
     
 }
+
+void Display()
+{
+    cout << endl;
+    cout << "===============" << endl;
+    cout << "Total Pass = " << n - 1 << endl;
+    cout << "===============" << endl;
+    cout << "Elelment Array yang telah tersusun" << endl;
+    cout << "===============" << endl;
+
+    for (int j = 0; j < n; j++)
+    {
+        cout << arr[j] << endl;
+    }
+    cout << endl;
+}
